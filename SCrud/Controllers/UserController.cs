0b07437a638636c72d981fc2bbb263ae3b2b8596
aissatAbderrahmane <abderrahmanekaddour.aissat@gmail.com>
2022-09-13 21:13:45 +0100

    User Profile 50%

--- SCrud/Controllers/UserController.cs
+++ SCrud/Controllers/UserController.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SCrud.Data;
+using SCrud.Models;
 
 namespace SCrud.Controllers
 {
     public class UserController : Controller
     {
+
+        private readonly AppDataContext context;
+
+        public UserController(AppDataContext context)
+        {
+            this.context = context;
+        }
+
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Profile(int Id)
+        {
+            Users user = context.Users.FirstOrDefault(x => x.Id == Id);
+            return View(user);
         }
     }
 }
